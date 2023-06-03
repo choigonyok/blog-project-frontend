d5e56876b9b71c8e14b294fd6e91484cf963c3a7
choigonyok <achoistic98@naver.com>
2023-06-03 22:23:36 +0900

    FEAT : NAV바 TECHLOG/ABOUT버튼 HOME/GITHUB PAGE로 라우팅 구현

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,26 @@
-import "./Header.css"
+import "./Header.css";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const homePageHandler = () => {
+    // 버튼 클릭 시 특정 URL로 이동
+    navigate("/");
+  };
+
+  const githubPageHandler = () => {
+    // 버튼 클릭 시 특정 URL로 이동
+    navigate("https://github.com/choigonyok");
+  };
+
   return (
     <div className="header">
-      <button className="header-title">_techlog</button>
+      <button className="header-title" onClick={homePageHandler}>_techlog</button>
       <div className="header-empty" />
       <button className="header-category">edit</button>
       <button className="header-category">write</button>
-      <button className="header-category">about</button>
+      <a href="https://github.com/choigonyok" className="header-category">about</a>
     </div>
   );
 };
