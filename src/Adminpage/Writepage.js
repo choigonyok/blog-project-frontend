03a447d54a08b7edbb2ee4fb1e84478d23d9986f
choigonyok <achoistic98@naver.com>
2023-06-25 03:55:59 +0900

    REFACTOR : 불필요한 코드 삭제

--- src/Adminpage/Writepage.js
+++ src/Adminpage/Writepage.js
@@ -21,7 +21,7 @@ const Writepage = () => {
   const navigate = useNavigate();
   const mounted = useRef(false);
 
-  const postHandler = (e) => {
+  const postHandler = () => {
     const postdata = {
       title: titleText,
       tag: tagText,
@@ -38,7 +38,7 @@ const Writepage = () => {
       })
       .catch((error) => {
         if (error.response.status === 400) {
-          alert(`특수문자 ' 가 입력된 곳이 존재합니다. 수정해주세요.`)
+          alert(`특수문자 ' 가 입력된 곳이 존재하거나 빈 칸이 존재합니다. 수정해주세요.`)
           console.error(error);
         } else if (error.response.status === 401) {
             console.error(error);
